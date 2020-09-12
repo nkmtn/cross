0f6f19cbc92651c7e21d21215d3f739596cb9726
nkmtn <nkmtn10@gmail.com>
2020-09-12 22:53:17 +0300

    Создан прототип меню

--- 3-electron/index.js
+++ 3-electron/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, dialog, BrowserWindow, Menu } = require('electron')
 
 function createWindow () {
   // Создаем окно браузера.
@@ -9,6 +9,39 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open...',
+          click() {
+            dialog.showOpenDialog({
+              title: 'Выберите файл в формате JSON',
+              properties: ['openFile']
+            }).then(result => {
+              console.log(result.canceled)
+            })
+          },
+          accelerator: 'CmdOrCtrl+O'
+        },
+        {
+          label: 'Save...',
+          accelerator: 'CmdOrCtrl+S'
+        },
+        {type:'separator'},
+        {
+          label: 'Quit',
+          click() {
+            app.quit()
+          },
+          accelerator: 'CmdOrCtrl+Q'
+        }
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
 
 // и загружаем index.html в приложении.
   win.loadFile('index.html')
