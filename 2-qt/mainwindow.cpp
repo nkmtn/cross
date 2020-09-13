c9b7b1d7b6e894bef28f97ab8802f33af347f9fe
nkmtn <nkmtn10@gmail.com>
2020-09-13 21:14:51 +0300

    Реализация на qt

---
+++ 2-qt/mainwindow.cpp
@@ -0,0 +1,71 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_action_Open_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    "Выберите файл ключа",
+                                                    QString(),
+                                                    "JSON (*.json);;All Files (*);;");
+
+    if (fileName.isEmpty())
+        return;
+
+    QFile f(fileName);
+    if (!f.open(QFile::ReadOnly | QFile::Text)) {
+        return;
+    }
+
+    QTextStream in(&f);
+    ui->textEdit->setText(in.readAll());
+}
+
+void MainWindow::on_action_Save_triggered()
+{
+    QJsonDocument doc = QJsonDocument::fromJson(ui->textEdit->toPlainText().toUtf8());
+
+    if (doc.isNull()) {
+        QMessageBox errorMessageBox(this);
+        errorMessageBox.setText(tr("Кривой JSON!"));
+        errorMessageBox.setStandardButtons(QMessageBox::Ok);
+        errorMessageBox.setDefaultButton(QMessageBox::Ok);
+        errorMessageBox.setIcon(QMessageBox::Warning);
+        errorMessageBox.setWindowTitle(tr("Error"));
+        errorMessageBox.exec();
+        return;
+    }
+
+    QString fileName = QFileDialog::getSaveFileName(this,
+                                                    "Выберите файл для сохранения результата",
+                                                    QString(),
+                                                    QString(),
+                                                    nullptr,
+                                                    QFileDialog::DontConfirmOverwrite);
+
+    QFile f(fileName);
+
+    if (!f.open(QFile::WriteOnly)) {
+        return;
+    }
+
+    QTextStream out(&f);
+    out << ui->textEdit->toPlainText();
+    f.close();
+}
+
+void MainWindow::on_action_Quit_triggered()
+{
+    QApplication::quit();
+}
---
+++ 2-qt/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
+#include <QJsonDocument>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void on_action_Open_triggered();
+    void on_action_Save_triggered();
+    void on_action_Quit_triggered();
+};
+#endif // MAINWINDOW_H
