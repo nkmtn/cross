f874a2e730746da6d96e07d1281e7b82a394fd9e
nkmtn <nkmtn10@gmail.com>
2020-10-09 13:10:29 +0300

    добавлено диалоговое окно для открытия файла по ссылке

--- 3-electron/index.js
+++ 3-electron/index.js
@@ -1,114 +1,110 @@
-const { app, dialog, ipcMain, BrowserWindow, Menu } = require('electron')
+const {app, dialog, ipcMain, BrowserWindow, Menu} = require('electron')
 const fs = require('fs');
 const path = require('path');
 
-// Открываем диалог для выбара одного файла с фильтрацией на json
-function createOpenDialog(win) {
-  dialog.showOpenDialog({
-    title: 'Выберите файл в формате JSON',
-    properties: ['openFile'],
-    filters: [
-      { name: 'Файлы JSON', extensions: ['json'] },
-      { name: 'Все файлы', extensions: ['*'] }
-    ]
-  }).then(result => {
-    // console.log(result.canceled)
-    // console.log(result.filePaths)
-    let data = fs.readFileSync(path.resolve(__dirname, result.filePaths[0])).toString();
-    win.webContents.send('open', data)
-  }).catch(err => {
-    console.log(err)
-  })
-}
-
-// Открываем диалог для сохранения json при условии валидности
-function createSaveDialog(arg) {
-  console.log(arg)
-  try {
-    JSON.parse(arg)
-  } catch (err) {
-    if (err instanceof SyntaxError) {
-      dialog.showErrorBox("Ошибка","Кривой JSON")
-    }
-    return
-  }
-
-  dialog.showSaveDialog({
-
-  }).then(result => {
-    if (result.canceled) {
-      return
-    }
-    fs.writeFileSync(result.filePath, arg)
-  })
-}
-
-function createWindow () {
-  // Создаем окно браузера.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  ipcMain.on('save', (event, arg) => {
-    createSaveDialog(arg)
-  })
-
-  // Задаём меню
-  const menu = Menu.buildFromTemplate([
-    {
-      label: 'File',
-      submenu: [
-        {
-          label: 'Open...',
-          click() {
-            createOpenDialog(win)
-          },
-          accelerator: 'CmdOrCtrl+O'
-        },
-        {
-          label: 'Save...',
-          click() {
-            win.webContents.send('save')
-          },
-          accelerator: 'CmdOrCtrl+S'
-        },
-        {type:'separator'}, // Разделяем элементы меню по приоритету
-        {
-          label: 'Quit',
-          click() {
-            app.quit()
-          },
-          accelerator: 'CmdOrCtrl+Q'
+function createWindow() {
+    // Создаем окно браузера.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            minimumFontSize: 16,
+            defaultFontSize: 16,
+            defaultMonospaceFontSize: 16
         }
-      ]
-    }
-  ]);
-  Menu.setApplicationMenu(menu);
+    })
 
-// и загружаем index.html в приложении.
-  win.loadFile('index.html')
-  
-  // Отображаем средства разработчика.
-//   win.webContents.openDevTools()
+    // win.webContents.openDevTools();
+
+    ipcMain.on('save', (event, arg) => {
+        console.log(arg)
+        try {
+            JSON.parse(arg)
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                dialog.showErrorBox("Ошибка","Кривой JSON")
+            }
+            return
+        }
+
+        dialog.showSaveDialog({
+
+        }).then(result => {
+            if (result.canceled) {
+                return
+            }
+            fs.writeFileSync(result.filePath, arg)
+        })
+    })
+
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Open...',
+                    click() {
+                        dialog.showOpenDialog({
+                            title: 'Выберите файл в формате JSON',
+                            properties: ['openFile'],
+                            filters: [
+                                { name: 'Файлы JSON', extensions: ['json'] },
+                                { name: 'Все файлы', extensions: ['*'] }
+                            ]
+                        }).then(result => {
+                            console.log(result.canceled)
+                            console.log(result.filePaths)
+                            let data = fs.readFileSync(path.resolve(__dirname, result.filePaths[0])).toString();
+                            win.webContents.send('open', data)
+
+                        }).catch(err => {
+                            console.log(err)
+                        })
+                    },
+                    accelerator: 'CmdOrCtrl+O'
+                },
+                {
+                    label: 'Open URL...',
+                    click() {
+                        win.webContents.send('open_url')
+                    },
+                    accelerator: 'CmdOrCtrl+L'
+                },
+                {
+                    label: 'Save...',
+                    click() {
+                        win.webContents.send('save')
+                    },
+                    accelerator: 'CmdOrCtrl+S'
+                },
+                {
+                    label: 'Quit',
+                    click() {
+                        app.quit()
+                    },
+                    accelerator: 'CmdOrCtrl+Q'
+                }
+            ]
+        }
+    ]);
+    Menu.setApplicationMenu(menu);
+
+    win.loadFile('index.html')
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
 
-
-app.whenReady().then(createWindow) 
+app.whenReady().then(createWindow)
